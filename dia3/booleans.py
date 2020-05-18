2d9ba838da7c314db81a8608598f107811c59014
m_del_mar <maria1.22dm@gmail.com>
2020-05-18 15:44:07 -0600

    .

---
+++ dia3/booleans.py
@@ -0,0 +1,15 @@
+first = True
+second = False
+
+if first == second:
+    print('si son iguales')
+else:
+    print('no son iguales')
+
+    #first = 5
+#second = 10
+
+#if first < second:
+   # print ('si ')
+    #else:
+     #   print('no')
---
+++ dia3/comparoValores.py
@@ -0,0 +1,18 @@
+#first = 5
+#second = 10
+
+#if first < second:
+#    print ('si ')
+#elif first == second:
+#    print('Son inguales')
+#else:
+#    print('no')
+
+number = int(input('Ingrese un numero '))
+
+if number < 5:
+    print (str(number) + ' Es menor a 5')
+elif number == 5:
+    print (str(number) + ' Es igual a 5')
+if number > 5:
+    print (str(number) + ' Es mayor que 5')
