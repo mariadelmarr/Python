6f36ff890122e706e0d9a455cfde0a1727b96d10
m_del_mar <maria1.22dm@gmail.com>
2020-05-19 15:47:07 -0600

    .

--- dia3/compararValores.py
+++ dia3/compararValores.py
@@ -1,16 +1,36 @@
-number = int(input('Type the score. '))
+# number = int(input('Type the score. '))
 
-if number >= 90 and number <= 100:
-    print ('Your score is A. ')
+# if number >= 90 and number <= 100:
+#     print ('Your score is A. ')
 
-elif number >= 80 and number <= 89:
-    print ('Your score is B. ')
+# elif number >= 80 and number <= 89:
+#     print ('Your score is B. ')
 
-elif number >= 70 and number <= 79:
-    print ('Your score is C. ')
+# elif number >= 70 and number <= 79:
+#     print ('Your score is C. ')
 
-elif number >= 60 and number <= 69:
-    print ('Your score is D. ')
+# elif number >= 60 and number <= 69:
+#     print ('Your score is D. ')
+
+# else:
+#      print ('Your score is F. ')
+
+number = float(input('Type the score. '))
+
+if number >= 90:
+    letter = 'A'
+    
+elif number >= 80:
+    letter = 'B'
+
+elif number >= 70:
+    letter = 'C'
+
+elif number >= 60:
+    letter = 'D'
 
 else:
-     print ('Your score is F. ')
+      letter = 'F'
+
+print ('Your score is: ' + letter)
+     
