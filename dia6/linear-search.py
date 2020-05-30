112808d4bf2d744a6aa0c7421023eb31735c3517
m_del_mar <maria1.22dm@gmail.com>
2020-05-29 20:32:57 -0600

    linear-serach

--- dia6/linear-search.py
+++ dia6/linear-search.py
@@ -1,4 +1,12 @@
-chars = ['d', 't', 'a', 'r', 'z', 'y']
-char = 'z'
+chars = ['d', 't', 'a', 'r', 'z', 'y', 'x', 'b']
+char = input('Que quiere buscar? ')
 
-for i in [1,2,3,4]:
+# Recorro la lista para buscar un indice
+for i in range(len(chars)):
+    if chars[i] == char:
+        print('Encontrado en el indice: ', i)
+        break #frena el for
+else:
+    print('No fue encontrado')
+print('fin...')
+
